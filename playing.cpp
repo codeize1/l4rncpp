0a54cd1b9cc3f0854ed276fe8c99cae61061ba03
melano <105978905+melqtx@users.noreply.github.com>
2024-05-30 10:06:06 +0530

    Update playing.cpp

--- playing.cpp
+++ playing.cpp
@@ -1,13 +1,1 @@
-// We don't need to #include <iostream> since main.cpp doesn't use any input/output functionality
-
-// These are the forward declarations for the functions in io.cpp
-int readNumber();
-void writeAnswer(int x);
-
-int main()
-{
-    int x{readNumber()};
-    int y{readNumber()};
-    writeAnswer(x + y);
-    return 0;
-}
+just a wrapper file
